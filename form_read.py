3ff39b3216831d1ffe306d515b5407d6d853fe04
Kevlyn Kadamala <kevlyn@gmail.com>
2019-08-22 14:21:52 +0530

    Added lot of data

---
+++ form_read.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('test_drive_form.csv')
+new_df = df.iloc[:,1]
+for index in range(2, df.shape[1]):
+    new_df = new_df.append(df.iloc[:,index], ignore_index=True)
+    
+new_df = new_df.dropna()
+new_df = pd.DataFrame(new_df)
+new_df.rename(columns={0: 'text'}, 
+                 inplace=True)
+
+new_df['label'] = np.full(new_df.shape[0], 1, dtype='int')
+new_df.to_csv('form_testdrive.csv', index=False)
